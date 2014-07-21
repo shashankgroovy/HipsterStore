c50054994131388c0d4b08b23357fe2916970a6f
Shashank Srivastav <shashankgrovy@gmail.com>
2014-07-21 09:22:19 +0530

    Explain the use of envvariables in init file

--- HipsterStore/__init__.py
+++ HipsterStore/__init__.py
@@ -2,9 +2,11 @@ import os
 from flask import Flask
 
 
-# create are awesome application
+# create our awesome application
 app = Flask(__name__)
 app.config.from_object(__name__)
+
+# TODO: figure out how to import configs
 #app.config.from_pyfile('config.py')
 
 # set the environment variable on your Linux
@@ -12,6 +14,8 @@ app.config.from_object(__name__)
 app.config.from_envvar('HIPSTER_SETTINGS', silent=True)
 
 # Get the views
+# To avoid 'Circular Imports' b/w __init__.py and views.py,
+# the views have been imported at the end of file.
 import HipsterStore.views
 
 if __name__=='__main__':
